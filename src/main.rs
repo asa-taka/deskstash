3e3c3d1e82be60b11b29a5027c9ef28206669fac
asataka <e70502@gmail.com>
2022-01-28 01:34:36 +0900

    Implement home dir detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::path::PathBuf;
+use chrono::prelude::{DateTime, Local, Utc};
+use dirs;
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(parse(from_os_str))]
+    dir: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let mut dir = args.dir;
+    let stash_root = match std::env::var("DESKSTASH_DIR") {
+        Ok(path) => PathBuf::from(path),
+        Err(e) => dirs::home_dir().unwrap()
+    };
+    let stash_today = stash_root.join(Local::now().format("%Y-%m-%d").to_string());
+
+    println!("{:?}", stash_today.into_os_string().into_string());
+
+    // TODO: confirm today's stashdir/YYYY-MM-DD existence
+    // if exist, make stashdir/YYYY-MM-DD.1
+
+    // TODO: move ~/Desktop/* to stashdir
 }
