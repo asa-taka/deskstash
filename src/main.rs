1dd25cca42c012f2a51ba2111ccec425a35654d3
asataka <e70502@gmail.com>
2022-01-29 20:53:53 +0900

    Add open command

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,71 @@
 #![allow(unused)]
 
 use std::fs;
+use std::io;
 use std::path::PathBuf;
 
 use chrono::prelude::{DateTime, Local, Utc};
-use clap::Parser;
+use clap::{AppSettings, Parser, Subcommand};
 use dirs;
 
 #[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+#[clap(global_setting(AppSettings::ArgsNegateSubcommands))]
 struct Cli {
-    #[clap(parse(from_os_str))]
-    dir: PathBuf,
+    #[clap(subcommand)]
+    command: Option<Command>,
 }
 
-fn main() -> Result<(), std::io::Error> {
-    let args = Cli::parse();
-    let mut dir = args.dir;
-    let stash_root = match std::env::var("DESKSTASH_DIR") {
+#[derive(Subcommand)]
+enum Command {
+    /// Open stash directory
+    Open {},
+    /// Stash desktop files
+    Stash {},
+}
+
+fn get_stash_root() -> PathBuf {
+    match std::env::var("DESKSTASH_DIR") {
         Ok(path) => PathBuf::from(path),
         Err(e) => dirs::home_dir()
             .expect("Home dir cannot detected.")
             .join(".deskstash"),
-    };
-    let stash_today = stash_root.join(Local::now().format("%Y-%m-%d-%H%M%S").to_string());
+    }
+}
+
+fn get_stash_dir() -> PathBuf {
+    get_stash_root().join(Local::now().format("%Y-%m-%d-%H%M%S").to_string())
+}
+
+fn open() -> io::Result<()> {
+    std::process::Command::new("open")
+        .arg(get_stash_root())
+        .spawn()
+        .expect("failed to execute process");
+    Ok(())
+}
+
+fn stash() -> io::Result<()> {
+    let dest_dir = get_stash_dir();
     let desktop = dirs::desktop_dir().expect("Desktop dir cannot detected.");
 
-    fs::create_dir_all(&stash_today)?;
+    fs::create_dir_all(&dest_dir)?;
     for e in fs::read_dir(desktop)? {
         let e = e?;
         let src = e.path();
-        let dest = stash_today.join(e.file_name());
+        let dest = dest_dir.join(e.file_name());
         println!("{:?} → {:?}", src, dest);
         fs::rename(src, dest)?
     }
     Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let args = Cli::parse();
+    match args.command.unwrap_or(Command::Stash {}) {
+        Command::Open {} => open(),
+        Command::Stash {} => stash(),
+    }
 }
